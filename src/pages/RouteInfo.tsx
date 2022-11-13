e96b455557276b5757ae91c37a80b75d8177ea76
Vishaal Ram <vishr@DESKTOP-BNQ2JHU.localdomain>
2022-11-13 12:20:26 -0500

    fix

--- src/pages/RouteInfo.tsx
+++ src/pages/RouteInfo.tsx
@@ -33,6 +33,7 @@ export function RouteInfo({ setData, setPage, dirData, fulldata, data, setDirDat
             <Loader />
         )
     }
+    console.log(labels);
     return (
         <div style={{width: '100%', height: '100%'}}>
             <Group position="apart" grow>
